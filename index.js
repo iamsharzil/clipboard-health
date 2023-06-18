bd64ac90f3853e6db2b34a8814dbbd6b897d5ae8
sharzilc24 <sharzil.shaikh@cars24.com>
2023-06-18 19:28:17 +0530

    verified different cases for deterministicPartitionKey function and logged them

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const {deterministicPartitionKey} = require("./dpk");
+const { deterministicPartitionKey } = require("./dpk");
 
 console.log(deterministicPartitionKey());
+
+console.log(
+  deterministicPartitionKey({
+    partitionKey: "abc123",
+  })
+);
+
+console.log(
+  deterministicPartitionKey({
+    data: "xyz456",
+  })
+);
+
+console.log(
+  deterministicPartitionKey({
+    partitionKey: 123,
+  })
+);
+
+console.log(
+  deterministicPartitionKey({
+    partitionKey: "veryLongPartitionKey".repeat(20),
+  })
+);
